42033b158df14ada328a4be897a397683d9769bd
Richlee2016 <345642459@qq.com>
2018-03-16 10:06:51 +0800

    0316

--- server/config/index.js
+++ server/config/index.js
@@ -1,3 +1,4 @@
 module.exports= {
-    db: 'mongodb://localhost:27017/koaapi',
+    // db: 'mongodb://localhost:27017/koaapi',
+    db:'mongodb://rich_movie:richmovie345642459@127.0.0.1:18888/richmovie'
 }
